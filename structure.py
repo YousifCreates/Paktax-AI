c43cb904fe633a768fcc8f4e9c85947cbaa926b0
Muhammad Yousif <yousifcreates@gmail.com>
2026-04-25 01:44:45 +0500

    Initial clean project setup

---
+++ structure.py
@@ -0,0 +1,25 @@
+import os
+
+def print_tree(root_dir, prefix=""):
+    entries = sorted(os.listdir(root_dir))
+    entries_count = len(entries)
+
+    for index, entry in enumerate(entries):
+        path = os.path.join(root_dir, entry)
+        connector = "└── " if index == entries_count - 1 else "├── "
+
+        print(prefix + connector + entry)
+
+        if os.path.isdir(path):
+            extension = "    " if index == entries_count - 1 else "│   "
+            print_tree(path, prefix + extension)
+
+
+def generate_structure(root_dir):
+    print(os.path.basename(root_dir) + "/")
+    print_tree(root_dir)
+
+
+if __name__ == "__main__":
+    folder_path = "/media/yousifcreates/339b14bb-e7ae-4348-b1dd-b0b8a896b600/Portfolio/Generative AI/AI with Sir Yasir/Paktax AI/Paktax-AI"
+    generate_structure(folder_path)
